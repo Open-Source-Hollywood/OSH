57489fdc7b7391db1a2671ae8b929322609e66ea
Reza Fatahi <aug2uag@gmail.com>
2017-11-17 23:56:28 -0800

    splash copy upgrade

--- server/stripe.js
+++ server/stripe.js
@@ -1,9 +1,3 @@
 Meteor.startup(function() {
-	ShareIt.configure({
-        sites: {
-            'facebook': {
-                'appId': '1790348544595983'
-            }
-        }
-    });
+	
 });
