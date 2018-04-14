6cfae590ab56428e242db5fa268527fb9de8a008
Reza Fatahi <aug2uag@gmail.com>
2018-04-14 03:07:51 -0700

    header optimizations, remove unused user conn, mark as fulfilled gifts tabled, and few other optimizations

--- server/publications/users.js
+++ server/publications/users.js
@@ -3,33 +3,6 @@
 * To make this available on the client, use a reactive cursor,
 * such as by creating a publication on the server:
 */
-Meteor.publish('connectUser', function() {
-
-    var user = Users.findOne(this.userId);
-
-    // if user then ready subscribe
-    if (user) {
-
-        // status offline then
-        if (!user.status) {
-
-            // User profile.status update online
-            Users.update(this.userId, { $set: { 'status': 'online' }});
-        }
-
-        // user close subscribe onStop callback update user.profile.status 'offline'
-        this.onStop(function() {
-
-            // update offline user
-            Users.update(this.userId, { $set: { 'status': false }});
-        });
-    }
-
-
-    // subscribe ready
-    this.ready();
-});
-
 Meteor.publish('profile', function(_id) {
     check(_id, Object);
     return Users.find({ '_id': _id });
